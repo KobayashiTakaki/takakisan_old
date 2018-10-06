f7b6d00efb9544db9e8fe54532759b415fee6740
小林鷹生 <takaki@kobayashi-takaki-macbook-pro.local>
2018-10-06 19:43:31 +0900

    mod stylesheet link

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
     </script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png" />
-    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>?20181006">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <?php wp_head(); ?>
